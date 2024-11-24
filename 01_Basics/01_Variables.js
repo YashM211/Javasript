e10810e85aafe806bb119dba6f821ac972cdbaaa
Yash <ymohta21@gmail.com>
2024-11-24 15:21:07 +0530

    Some Basics

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 33434
+let accountEmail = "ymohta@gmail.com"
+var accountPassword = "12345"
+ accountCity="Jaipur" ;
+let accountState ;
+//accountId=2
+/*
+    Prefer not to use var 
+    because of block and global scope
+*/
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all code as New version of Javascript
+
+// alert(3+3) We are using nodejs not browser
+
+let name = "yash"
+let age = 23
+let isLoggedIn = false
+
+
+//number =>2 to power 53
+//bigint
+//string = ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+console.log(typeof(age)) //Number
+console.log(typeof(null)) //Object
+console.log(typeof(undefined)); //Undefined
